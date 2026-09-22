83030b82c07e062b8d660f3e9513f7ff2bf000d6
anubrotaguhaneogi-wq <anubrotaguhaneogi@gmail.com>
2026-09-22 16:29:42 +0530

    Add real Firebase configuration

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,20 @@
 // Firebase Configuration & Initialization
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-// আপনার Firebase কনফিগারেশন এখানে বসবে
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "এখানে_তোমার_API_KEY",
+  authDomain: "এখানে_তোমার_AUTH_DOMAIN",
+  projectId: "এখানে_তোমার_PROJECT_ID",
+  storageBucket: "এখানে_তোমার_STORAGE_BUCKET",
+  messagingSenderId: "এখানে_তোমার_MESSAGING_SENDER_ID",
+  appId: "এখানে_তোমার_APP_ID",
+  measurementId: "এখানে_তোমার_MEASUREMENT_ID"
 };
 
-// Firebase Initialize করা
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
